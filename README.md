3fcf588643a9783ca5238fabfbb538f8c89f14f9
objectorange <objectorange@protonmail.com>
2019-06-17 12:37:29 -0400

    update description

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # 1m5-ring
-A decentralized metric-based reputational protocol.
+A decentralized metric-based reputation protocol.
--- src/main/java/io/onemfive/ring/RingService.java
+++ src/main/java/io/onemfive/ring/RingService.java
@@ -3,7 +3,7 @@ package io.onemfive.ring;
 import io.onemfive.core.BaseService;
 
 /**
- *
+ * A decentralized metric-based reputation protocol as a service.
  * @author objectorange
  */
 public class RingService extends BaseService {
